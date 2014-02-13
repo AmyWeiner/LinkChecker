4afeafdccdc95a45b6703e06a6b5bcaad779a1df
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-13 15:16:24 -0800

    fixes all original failures

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -11,6 +11,7 @@ class SitesController < ApplicationController
       f.html { redirect_to site_path(@site) }
       f.json { render :json => @site }
     end
+
   end
 
   def show
@@ -19,7 +20,7 @@ class SitesController < ApplicationController
     @links = @site.links
     respond_to do |f|
       f.html
-      f.json { render :json => @site }
+      f.json { render :json => @site.as_json(include: :links) }
     end
   end
 
@@ -35,6 +36,22 @@ class SitesController < ApplicationController
     end
   end
 
+  rescue_from ActiveRecord::RecordNotFound, :only => :new do |err|
+    respond_to do |f|
+      f.html do 
+        redirect_to new_site_path
+      end
+      f.json {render :json  => {:error => err.message}, :status => 404}
+    end
+  end
+
+  rescue_from ActionView::MissingTemplate do |exception|
+    # use exception.path to extract the path information
+    # This does not work for partials
+    redirect_to '/sites/new'
+  end
+
+  
   # rescue_from ActionController::ParameterMissing, :handle_create_param_missing :only => :create
   #
   # def handle_create_param_missing
